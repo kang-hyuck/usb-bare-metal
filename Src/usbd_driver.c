fc97d277e4110a9e327151c4455a4045b628a652
kang-hyuck <kang-hyuck@naver.com>
2021-01-24 13:24:00 +0900

    feat: add initialize_core(enable USB core,interrupts, configure device mode

--- Src/usbd_driver.c
+++ Src/usbd_driver.c
@@ -1,10 +1,10 @@
 #include "usbd_driver.h"
 
 
-void initialize_usb_pins()
+void initialize_gpio_pins()
 {
 	/* Enable Clock for GPIOB */
-	SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOBEN);
+	SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOBEN);  // Set GPIOBEN Region
 
 	/* Select AFR(alternate function register) GPIOB14(OTG D-), GPIOB15(OTG D+) */
 	MODIFY_REG(GPIO->AFR[1],   // AFR[1]:High,  AFR[0]:LOW
@@ -19,4 +19,38 @@ void initialize_usb_pins()
 	);
 }
 
+
+void initialize_core()
+{
+	/* Enable Clock for USB core */
+	SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_OTGHSEN);  // Set OTGHSEN Region
+
+	/* Set USB core to device mode, full-speed PHY */
+	MODIFY_REG(USB_OTG_HS->GUSBCFG,
+			USB_OTG_GUSBCFG_FDMOD | USB_OTG_GUSBCFG_PHYSEL | USB_OTG_GUSBCFG_TRDT,
+			USB_OTG_GUSBCFG_FDMOD | USB_OTG_GUSBCFG_PHYSEL | _VAL2FLD(USB_OTG_GUSBCFG_TRDT, 0x09)
+	);
+
+	/* device initialize section (Configure device speed) */
+	MODIFY_REG(USB_OTG_HS_DEVICE->DCFG,
+			USB_OTG_DCFG_DSPD,
+			_VAL2FLD(USB_OTG_DCFG_DSPD, 0x03)
+	);
+
+	/* Enable VBUS sensing device */
+	SET_BIT(USB_OTG_HS->GCCFG, USB_OTG_GCCFG_VBUSBSEN);
+
+	/* Unmask USB core interrupt bits */
+	SET_BIT(USB_OTG_HS->GINTMSK,
+		USB_OTG_GINTMSK_USBRST | USB_OTG_GINTMSK_ENUMDNEM | USB_OTG_GINTMSK_SOFM |
+		USB_OTG_GINTMSK_USBSUSPM | USB_OTG_GINTMSK_WUIM | USB_OTG_GINTMSK_IEPINT |
+		USB_OTG_GINTSTS_OEPINT | USB_OTG_GINTMSK_RXFLVLM
+	);
+
+	/* Clear all pending registers */
+	WRITE_REG(USB_OTG_HS->GINTSTS, 0xFFFFFFFF);
+
+	/* Unmasks "USB global interrupt" which will activate all unmasked interrupts */
+	SET_BIT(USB_OTG_HS->GAHBCFG, USB_OTG_GAHBCFG_GINT);
+}
 
